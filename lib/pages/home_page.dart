01b487740fb08d7d81599e014d15fd2605fc6bf8
Eric Han <eric_han@emf.com.sg>
2023-12-10 16:58:04 +0630

    feat(): finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:todo_app/models/todo.dart';
 import 'package:todo_app/pages/add_page.dart';
+import 'package:todo_app/providers/todo_provider.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({
     super.key,
   });
   @override
-  State<HomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<Todo> todos = ref.watch(todoProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Todo app"),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (context, index) {
+            return Slidable(
+              child: ListTile(title: Text(todos[index].content)),
+              startActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  SlidableAction(
+                    onPressed: (context) {
+                      ref.watch(todoProvider.notifier).deleteTodo(todos[index]);
+                    },
+                    icon: Icons.delete,
+                    backgroundColor: Colors.red,
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  )
+                ],
+              ),
+              endActionPane: ActionPane(
+                motion: const ScrollMotion(),
+                children: [
+                  SlidableAction(
+                    onPressed: (context) {
+                      ref.watch(todoProvider.notifier).completeTodo(index);
+                    },
+                    icon: Icons.check,
+                    backgroundColor: Colors.green,
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  )
+                ],
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context)
